8ff5fdca4b2a4bc9650faa621becabed4045831d
Dharmik <hingudharmik4@gmail.com>
2022-03-06 15:20:49 +0530

    add catergory in schema

--- models/application.js
+++ models/application.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const applicationSchema = new mongoose.Schema({
 	title: { type: [String, 'Title must be String'] },
 	description: { type: [String, 'Description must be String'] },
+	category: {
+		type: [String, 'Please Select a valid category'],
+		enum: ['Medical Leave', 'Other'],
+	},
 })
 
 module.exports = mongoose.model('Application', applicationSchema)
